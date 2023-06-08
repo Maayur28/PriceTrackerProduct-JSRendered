c9a80e49898cd37c415a9a9aada3e3d86f0e1ac9
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-06-08 21:14:42 +0530

    added fix

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const getUserAgents = require("./utilities/useragents");
+const cheerio = require("cheerio");
 
 const app = require("express")();
 
@@ -26,23 +27,88 @@ app.get("/api", async (req, res) => {
   }
 
   try {
-    let browser = await puppeteer.launch(options);
-
-    let page = await browser.newPage();
-    let userAgent = getUserAgents();
-    await page.setUserAgent(userAgent);
-    await page.goto(
-      "https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy",
-      {
-        waitUntil: "domcontentloaded",
+    let retry = 0;
+    do {
+      console.log("retrying... ", retry);
+      let browser = await puppeteer.launch(options);
+      let page = await browser.newPage();
+      let userAgent = getUserAgents();
+      await page.setUserAgent(userAgent);
+      await page.goto(
+        "https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy",
+        {
+          waitUntil: "domcontentloaded",
+        }
+      );
+      let $ = cheerio.load(await page.content());
+      await browser.close();
+      if ($ && $(".pdp-name").html() != null) {
+        console.log($(".pdp-name").html());
+        let response = fetchMyntra(
+          $,
+          "https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy",
+          "MYNTRA"
+        );
+        res.json({ response: await response }).status(200);
+      } else {
+        retry++;
       }
-    );
-    res.send(await page.title());
+    } while (retry <= 5);
   } catch (err) {
-    console.error(err);
-    return null;
+    console.error(err.message);
   }
 });
+
+const fetchMyntra = async ($, URL, domain) => {
+  let response = {};
+
+  //title
+  response.title = $(".pdp-name").html().trim();
+
+  //price
+  let price = {};
+  price.originalPrice = $(".pdp-mrp > s").text().trim();
+  price.discountPrice = $(".pdp-price > strong").html().trim();
+  if (price.originalPrice.length > 0 && price.originalPrice.charAt(0) == "₹") {
+    price.originalPrice = price.originalPrice.slice(1);
+  }
+  if (price.discountPrice.length > 0 && price.discountPrice.charAt(0) == "₹") {
+    price.discountPrice = price.discountPrice.slice(1);
+  }
+  response.price = price;
+
+  //image
+  let imageHtml = cheerio.load(
+    $("div.image-grid-col50:nth-of-type(1) > .image-grid-imageContainer").html()
+  );
+  response.image = imageHtml(".image-grid-image")
+    .css("background-image")
+    .replace(/(url\(|\)|")/g, "");
+
+  //badge
+  if (
+    $(".xcelerator-pdpXceleratorImageTag").text() != "" &&
+    $(".xcelerator-pdpXceleratorImageTag").text() != null &&
+    $(".xcelerator-pdpXceleratorImageTag").text() != undefined
+  ) {
+    response.badge = $(".xcelerator-pdpXceleratorImageTag").text();
+  }
+
+  //rating
+  let rate = {};
+  let rating = $(".index-overallRating").text();
+  rate.ratingCount = rating.split("|")[0] + " out of 5 stars";
+  rate.totalRated = rating.split("|")[1];
+  response.rating = rate;
+
+  //domain
+  response.domain = domain;
+
+  //url
+  response.url = URL;
+
+  return response;
+};
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started");
