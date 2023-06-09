154c412394dad7937ef86f1b6caf15cdc0d2db0f
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-06-09 08:01:33 +0530

    added fix

--- index.js
+++ index.js
@@ -1,118 +1,19 @@
-const getUserAgents = require("./utilities/useragents");
-const cheerio = require("cheerio");
+const express = require("express");
+const { scrapeLogic } = require("./scrapeLogic");
+const app = express();
 
-const app = require("express")();
+const PORT = process.env.PORT || 4000;
 
-let chrome = {};
-let puppeteer;
-
-if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
-} else {
-  puppeteer = require("puppeteer");
-}
-
-app.get("/api", async (req, res) => {
-  let options = {};
-
-  if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-    options = {
-      args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chrome.defaultViewport,
-      executablePath: await chrome.executablePath,
-      headless: true,
-      ignoreHTTPSErrors: true,
-    };
-  }
-
-  try {
-    let retry = 0;
-    do {
-      console.log("retrying... ", retry);
-      let browser = await puppeteer.launch(options);
-      let page = await browser.newPage();
-      let userAgent = getUserAgents();
-      await page.setUserAgent(userAgent);
-      await page.goto(
-        "https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy",
-        {
-          waitUntil: "domcontentloaded",
-        }
-      );
-      let $ = cheerio.load(await page.content());
-      await browser.close();
-      if ($ && $(".pdp-name").html() != null) {
-        console.log($(".pdp-name").html());
-        let response = fetchMyntra(
-          $,
-          "https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy",
-          "MYNTRA"
-        );
-        res.json({ response: await response }).status(200);
-      } else {
-        console.log($);
-        retry++;
-      }
-    } while (retry <= 5);
-  } catch (err) {
-    console.error(err.message);
-  }
+app.get("/scrape", (req, res) => {
+  scrapeLogic(res);
 });
 
-const fetchMyntra = async ($, URL, domain) => {
-  let response = {};
+app.get("/", (req, res) => {
+  res.send("Render Puppeteer server is up and running!");
+});
 
-  //title
-  response.title = $(".pdp-name").html().trim();
-
-  //price
-  let price = {};
-  price.originalPrice = $(".pdp-mrp > s").text().trim();
-  price.discountPrice = $(".pdp-price > strong").html().trim();
-  if (price.originalPrice.length > 0 && price.originalPrice.charAt(0) == "₹") {
-    price.originalPrice = price.originalPrice.slice(1);
-  }
-  if (price.discountPrice.length > 0 && price.discountPrice.charAt(0) == "₹") {
-    price.discountPrice = price.discountPrice.slice(1);
-  }
-  response.price = price;
-
-  //image
-  let imageHtml = cheerio.load(
-    $("div.image-grid-col50:nth-of-type(1) > .image-grid-imageContainer").html()
-  );
-  response.image = imageHtml(".image-grid-image")
-    .css("background-image")
-    .replace(/(url\(|\)|")/g, "");
-
-  //badge
-  if (
-    $(".xcelerator-pdpXceleratorImageTag").text() != "" &&
-    $(".xcelerator-pdpXceleratorImageTag").text() != null &&
-    $(".xcelerator-pdpXceleratorImageTag").text() != undefined
-  ) {
-    response.badge = $(".xcelerator-pdpXceleratorImageTag").text();
-  }
-
-  //rating
-  let rate = {};
-  let rating = $(".index-overallRating").text();
-  rate.ratingCount = rating.split("|")[0] + " out of 5 stars";
-  rate.totalRated = rating.split("|")[1];
-  response.rating = rate;
-
-  //domain
-  response.domain = domain;
-
-  //url
-  response.url = URL;
-
-  return response;
-};
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server started");
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
 
 module.exports = app;
---
+++ scrapeLogic.js
@@ -0,0 +1,105 @@
+const puppeteer = require("puppeteer");
+const getUserAgent = require("./utilities/useragents");
+const cheerio = require("cheerio");
+require("dotenv").config();
+
+function delay(milliseconds) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
+const scrapeLogic = async (res) => {
+  const browser = await puppeteer.launch({
+    args: [
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
+    ],
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  try {
+    let retry = 0;
+    do {
+      const page = await browser.newPage();
+      let URL =
+        "https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy";
+      let userAgent = getUserAgent();
+      await page.setUserAgent(userAgent);
+      await page.goto(URL, {
+        waitUntil: "domcontentloaded",
+      });
+      let $ = cheerio.load(await page.content());
+      if ($ && $(".pdp-name").html() != null) {
+        let response = await fetchMyntra($, URL, "MYNTRA");
+        res.send(response);
+      } else {
+        await delay(2000);
+        retry++;
+      }
+    } while (retry <= 5);
+  } catch (e) {
+    console.error(e.message);
+    res.send(`Something went wrong while running Puppeteer: ${e}`);
+  } finally {
+    console.log("finally");
+    await browser.close();
+  }
+};
+
+const fetchMyntra = async ($, URL, domain) => {
+  let response = {};
+
+  //title
+  response.title = $(".pdp-name").html().trim();
+
+  //price
+  let price = {};
+  price.originalPrice = $(".pdp-mrp > s").text().trim();
+  price.discountPrice = $(".pdp-price > strong").html().trim();
+  if (price.originalPrice.length > 0 && price.originalPrice.charAt(0) == "₹") {
+    price.originalPrice = price.originalPrice.slice(1);
+  }
+  if (price.discountPrice.length > 0 && price.discountPrice.charAt(0) == "₹") {
+    price.discountPrice = price.discountPrice.slice(1);
+  }
+  response.price = price;
+
+  //image
+  let imageHtml = cheerio.load(
+    $("div.image-grid-col50:nth-of-type(1) > .image-grid-imageContainer").html()
+  );
+  response.image = imageHtml(".image-grid-image")
+    .css("background-image")
+    .replace(/(url\(|\)|")/g, "");
+
+  //badge
+  if (
+    $(".xcelerator-pdpXceleratorImageTag").text() != "" &&
+    $(".xcelerator-pdpXceleratorImageTag").text() != null &&
+    $(".xcelerator-pdpXceleratorImageTag").text() != undefined
+  ) {
+    response.badge = $(".xcelerator-pdpXceleratorImageTag").text();
+  }
+
+  //rating
+  let rate = {};
+  let rating = $(".index-overallRating").text();
+  rate.ratingCount = rating.split("|")[0] + " out of 5 stars";
+  rate.totalRated = rating.split("|")[1];
+  response.rating = rate;
+
+  //domain
+  response.domain = domain;
+
+  //url
+  response.url = URL;
+
+  return response;
+};
+
+module.exports = { scrapeLogic };
