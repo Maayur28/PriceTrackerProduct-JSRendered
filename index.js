f3a92354d31e6d17c23018dfa7b84dac589e6dfc
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-06-30 10:14:53 +0530

    added fix

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const express = require("express");
-const { scrapeLogic } = require("./scrapeLogic");
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", (req, res) => {
-  scrapeLogic(req.query.url, res);
+app.get("/scrape", async(req, res) => {
+  const browser = await puppeteer.connect({
+    browserWSEndpoint: `wss://${process.env.RAILWAY_STATIC_URL}/devtools/browser`,
+  });
+
+  // Create a new page
+  const page = await browser.newPage();
+
+  await page.goto("https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy");
+  
+  const title = await page.title();
+  console.log('Page title:', title);
+
+  // Close the browser
+  await browser.close();
 });
 
 app.get("/", (req, res) => {
