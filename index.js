1666cfa7adbef4312942ebb28eb608e6b7675122
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-06-30 10:37:43 +0530

    added catch

--- index.js
+++ index.js
@@ -7,24 +7,27 @@ require("dotenv").config();
 const PORT = process.env.PORT || 4000;
 
 app.get("/scrape", async (req, res) => {
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: `wss://${process.env.RAILWAY_STATIC_URL}/devtools/browser`,
-  });
+  try {
+    // Launch Puppeteer with the Railway Chrome instance
+    const browser = await puppeteer.connect({
+      browserWSEndpoint: `wss://${process.env.RAILWAY_STATIC_URL}/devtools/browser`,
+    });
 
-  // Create a new page
-  const page = await browser.newPage();
+    // Create a new page
+    const page = await browser.newPage();
 
-  await page.goto(
-    "https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy"
-  );
+    // Example: Navigate to a website and fetch the title
+    await page.goto(
+      "https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy"
+    );
+    const title = await page.title();
+    console.log("Page title:", title);
 
-  const title = await page.title();
-  console.log("Page title:", title);
-
-  res.send(await title).status(200);
-
-  // Close the browser
-  await browser.close();
+    // Close the browser
+    await browser.close();
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 });
 
 app.get("/", (req, res) => {
