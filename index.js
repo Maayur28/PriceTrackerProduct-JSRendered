aa6540a30eaf763bd1a18a988e0b9e1beb450f37
Mayur Agarwal <mayuragarwal2812@gmail.com>
2023-06-30 10:20:03 +0530

    added fix

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const { puppeteer } = require("puppeteer");
 const app = express();
 
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
 
-app.get("/scrape", async(req, res) => {
+app.get("/scrape", async (req, res) => {
   const browser = await puppeteer.connect({
     browserWSEndpoint: `wss://${process.env.RAILWAY_STATIC_URL}/devtools/browser`,
   });
@@ -13,10 +14,12 @@ app.get("/scrape", async(req, res) => {
   // Create a new page
   const page = await browser.newPage();
 
-  await page.goto("https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy");
+  await page.goto(
+    "https://www.myntra.com/sports-shoes/puma/puma-men-black-zeta-mesh-running-shoes/14463342/buy"
+  );
 
   const title = await page.title();
-  console.log('Page title:', title);
+  console.log("Page title:", title);
 
   // Close the browser
   await browser.close();
